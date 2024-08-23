15c4948a22eb958e07a642971621ddf43e26b8ea
Daniel Ives <dives@bamboorose.com>
2024-08-23 07:04:49 -0600

    feat: add local copilot

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -30,6 +30,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>cb', ':CodeCompanionChat<CR>', { desc = 'Open a chat' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
